4d1bbc0b49baecf907de331e9f0055e43b1f9781
Fei <feimosqueda777@gmail.com>
2024-07-13 19:55:01 -0300

    añadiendo todo

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="card">
+        <img src="https://avatars.githubusercontent.com/u/117238821?v=4" alt="" />
+        <div>
+          
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
